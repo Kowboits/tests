41bc9e5839e37e0c5f8b00a1bb30702f47d62118
kowboits <e.s.kapranov@yandex.ru>
2022-09-07 13:50:09 +0300

    initial commit

---
+++ test/test add_self.py
@@ -0,0 +1,16 @@
+from main import add_self
+import unittest
+from unittest import mock
+from parameterized import parameterized
+
+class TestName_serch(unittest.TestCase):
+
+    @parameterized.expand(([
+        ['2', 'Такая полка уже сужествует'],
+        ['4','Полка добавлена'],
+        ['1','Такая полка уже сужествует'],
+    ]))
+    def test_add_self(self, shelf, ans):
+        mock.builtins.input = lambda _: numb
+        mock.builtins.input = lambda _: shelf
+        self.assertEqual(add_self(), ans)
